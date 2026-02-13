33652221d1e99866e7698751a497f8e5b59d23a1
cheffedualonso-oss <cheffedualonso@gmail.com>
2026-02-13 00:49:33 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  }
+});
+
+client.on('qr', qr => {
+  qrcode.generate(qr, { small: true });
+  console.log('Escaneie o QR acima');
+});
+
+client.on('ready', () => {
+  console.log('Bot está online!');
+});
+
+client.on('message', async message => {
+  if (!message.from.endsWith('@g.us')) return;
+
+  if (message.body === '!oi') {
+    message.reply('Olá 👋');
+  }
+
+  if (message.body === '!menu') {
+    message.reply('🤖 Comandos:\n!oi\n!menu\n!musica');
+  }
+
+  if (message.body === '!musica') {
+    const musicas = [
+      'Shape of You - Ed Sheeran',
+      'Blinding Lights - The Weeknd',
+      'Dance Monkey - Tones and I'
+    ];
+    const random = musicas[Math.floor(Math.random() * musicas.length)];
+    message.reply('🎵 Música: ' + random);
+  }
+});
+
+client.initialize();
